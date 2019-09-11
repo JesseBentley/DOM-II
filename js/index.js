562f709258f2679fa14e7987c3bcfc8c73728d0c
Jesse Bentley <JesseDBentley@outlook.com>
2019-09-10 21:54:50 -0400

    Updating code as it wasn't working before - rewrote it to be more simple

--- js/index.js
+++ js/index.js
@@ -1,79 +1,88 @@
 // Your code goes here
-const navLinks = document.querySelectorAll('.nav a');
-const introLogo = document.querySelector('.intro img');
-const headerLogo = document.querySelector('.logo-heading');
-const textContent = document.querySelectorAll('.text-content');
-const imgContent = document.querySelectorAll('.img-content');
-const button = document.querySelectorAll('.btn');
-const paragraph = document.querySelectorAll('p');
-const pHeader2 = document.querySelectorAll('h2');
-const pHeader4 = document.querySelectorAll('h4');
-const destination = document.querySelectorAll('.destination')
-const nav = document.querySelector('nav');
 
-nav.event.preventDefault();
-const navHoverHandler = function (event) {
-  event.target.style.color = 'red';
-}
+let images = document.querySelectorAll('.home img');
 
-navLinks.forEach((hover) => {
-  hover.addEventListener('mouseover', navHoverHandler)
-})
+images.forEach(image => {
+  image.addEventListener('mouseenter', event => {
+    image.style.transform = 'scale(1.5)';
+    image.style.transition = 'transform 0.5s';
+    //TweenLite.to(image, 1, { x: 100, ease: Elastic.easeOut });
+  });
 
-const display = function (event) {
-  event.target.style.filter = 'grayscale(100%)';
-}
-
-introLogo.addEventListener('wheel', display);
-
-const click = function (event) {
-  window.open('https://en.wikipedia.org/wiki/School_bus')
-}
-
-headerLogo.addEventListener('click', click)
-
-const textShadow = function (event) {
-  event.target.style.textShadow = '10px 10px gray';
-}
-
-textContent.forEach((shadow) => {
-  shadow.addEventListener('dblclick', textShadow);
+  image.addEventListener('mouseleave', event => {
+    image.style.transform = 'scale(1.0)';
+    image.style.transition = 'transform 0.5s';
+  });
 });
 
-const shadowBox = function (event) {
-  event.target.style.boxShadow = ('5px 5px gray');
-}
-imgContent.forEach((boxShadow) => {
-  boxShadow.addEventListener('mouseover', shadowBox);
-})
+let textContents = document.querySelectorAll('.text-content');
 
-const btnScale = function (event) {
-  event.target.style.transform = 'scale(1, 1.5)'
-}
+textContents.forEach(text => {
+  text.addEventListener('click', event => {
+    text.style.background = 'silver';
+    text.style.color = 'white';
+  });
 
-button.forEach((sizeBtn) => {
-  sizeBtn.addEventListener('mouseover', btnScale);
-})
-
-const paragraphDisplay = function (event) {
-  event.target.style.display = 'none';
- 
-}
-paragraph.forEach((hide) => {
-  hide.addEventListener('mouseout', paragraphDisplay)
-})
-
-const pHeaders = function (event) {
-  event.target.style.fontSize = '6rem';
-}
-pHeader2.forEach((resize) => {
-  resize.addEventListener('mouseout', pHeaders)
+  text.addEventListener('dblclick', event => {
+    text.style.background = 'white';
+    text.style.color = 'black';
+  });
 });
-const destinationColor = function (event) {
+
+let anyPInHome = document.querySelectorAll('.home p');
+
+anyPInHome.forEach(para => {
+  para.addEventListener('copy', event => {
+    alert('I see you are copying some text, it will be CAPITALIZED!');
+    const selection = document.getSelection();
+    event.clipboardData.setData(
+      'text/plain',
+      selection.toString().toUpperCase()
+    );
+    event.preventDefault();
+  });
+});
+
+document.addEventListener('keydown', event => {
+  let keyHit = event.key;
+  let keyCody = event.keyCode;
+  alert(`You hit the ${keyHit} key, be more careful`);
+});
+
+window.addEventListener('resize', event => {
+  let newHeight = window.innerHeight;
+  let newWidth = window.innerWidth;
+
+  location.reload();
+
+  alert('Is it getting smaller in here, or is that just me?');
+  alert(`My new height is ${newHeight} and new width is ${newWidth}`);
+});
+
+let destinationButtons = document.querySelectorAll('.content-pick .btn');
+destinationButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    button.style.background = 'green';
+    alert('This trip is currently full');
+  });
+});
+
+//stop propagation
+let theFooterP = document.querySelector('.footer p');
+theFooterP.addEventListener('click', event => {
+  theFooterP.style.display = 'none';
   event.stopPropagation();
-  event.target.style.backgroundColor = 'yellow';
+});
 
-}
-destination.forEach((colors) => {
-  colors.addEventListener('mouseout', destinationColor);
-})
+let theFooter = document.querySelector('.footer');
+theFooter.addEventListener('click', event => {
+  theFooter.style.background = 'blue';
+  // event.stopPropagation();
+});
+
+let anchorTags = document.querySelectorAll('.nav-link');
+anchorTags.forEach(tag => {
+  tag.addEventListener('click', event => {
+    event.preventDefault();
+  });
+});
